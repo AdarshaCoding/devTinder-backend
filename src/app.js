462c84c8baaf24d4ad94785c3a40173537db4c0b
AdarshaCoding <adarshapc4467@gmail.com>
2025-03-18 20:16:11 +0530

    Added userAuth&adminAuth - Middle ware

--- src/app.js
+++ src/app.js
@@ -1,44 +1,25 @@
 const express = require("express");
-
+const { userAuth, adminAuth } = require("./middleware/auth");
 const PORT = 4000;
 const app = express();
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    const token = "xyz123";
-    const isUserAuthorized = token === "xyz12";
-    if (!isUserAuthorized) {
-      res.status(401).send("User is not authorized!");
-    } else {
-      next();
-    }
-  },
-  (req, res) => {
-    console.log("User get request from DB");
-    const user = {
-      name: "Adarsha",
-      age: "32",
-    };
-    res.send(user);
-  }
-);
+app.get("/user", userAuth, (req, res) => {
+  console.log("User get request from DB");
+  const user = {
+    name: "Adarsha",
+    age: "32",
+  };
+  res.send(user);
+});
+app.use("/admin", adminAuth);
 
-app.delete(
-  "/admin",
-  (req, res, next) => {
-    const token = "xyz123";
-    const isUserAuthorized = token === "xyz123";
-    if (!isUserAuthorized) {
-      res.status(401).send("User is not authorized!");
-    } else {
-      next();
-    }
-  },
-  (req, res) => {
-    res.send("User is deleted!");
-  }
-);
+app.get("/admin/getAllUsers", (req, res) => {
+  res.send("Take all users data");
+});
+
+app.delete("/admin/deleteUser", (req, res) => {
+  res.send("User is deleted!");
+});
 
 app.listen(PORT, () => {
   console.log("Sucessfully Server Started at Port#:", PORT);
