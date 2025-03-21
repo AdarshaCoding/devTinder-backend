2fc55cbfdb7a65b8906bc532dee4036377747e9a
AdarshaCoding <adarshapc4467@gmail.com>
2025-03-21 06:06:56 +0530

    Individual route handlers - GET and POST

--- src/test.js
+++ src/test.js
@@ -4,35 +4,21 @@ const PROT = 5000;
 const app = express();
 
 /**
- * Order of routes matters.
- * "/" - default routes should be kep at last only.
- * "/test", "/test/xyz", "/test/xyz/abc" -> here, "/test" route is kind of home/default routes, it should be kept at last else "/test" route only will get executed even after you provide any routes follwed by "test"
- * Routes Orders would be like this - this only individual routes will be exectuted
- *  "/test/xyz/abc"
- *   "/test/xyz"
- *   "/test"
- */
-
-/**
  * app.use("path", request_handler) : This will handle all the HTTMP methods like GET, POST, PATCH, PUT, DELETE
  * if we have to handle independently then use respective HTTP methods
  */
 
-app.use("/test/xyz/abc", (req, res) => {
-  res.send("xyz/abc route");
+//GET : http://localhost:5000/user
+app.get("/user", (req, res) => {
+  userObj = {
+    name: "Adarsha",
+    city: "Bengaluru",
+  };
+  res.send(userObj);
 });
 
-app.use("/test/xyz", (req, res) => {
-  res.send("xyz route");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Testing Route!!");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello Route!!");
-});
+//POST : http://localhost:5000/user
+// Now, if I execute POST method from postman, the below default route gets executed as we don't have "post" route handler
 
 app.use("/", (req, res) => {
   res.send("Home Page!!");
