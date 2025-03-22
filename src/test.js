ba561966fe811c477e55b4cc7d255b24c82aa1fb
AdarshaCoding <adarshapc4467@gmail.com>
2025-03-22 14:51:14 +0530

    Middleware and request handler

--- src/test.js
+++ src/test.js
@@ -1,40 +1,67 @@
 const express = require("express");
-const fs = require("node:fs");
 const PROT = 5000;
 
 const app = express();
 
-function getTimestamp() {
-  const now = new Date();
-  return `${now.toISOString()}`;
-}
+/**
+ * Middleware : the routes which helps (like authorization, field validations ...) to reach the route handler
+ * Request Handler: the route handler which sends the response back to client
+ * app.get("/path", rH1, rH2, rH3, rH4)
+ *  - rH1, rH2, rH3 => middlewares
+ *  - rH4 => request handler
+ *  - request handler can be grouped by []  => results will be same
+ *     - app.get("/path", [rH1, rH2, rH3, rH4]) or app.get("/path", rH1, [rH2, rH3], rH4)
+ */
 
-function logMessage(message) {
-  const logDetails = `${getTimestamp()} - ${message}\n`;
-  return logDetails;
-}
-
-app.get("/user", (req, res) => {
-  res.send("Hello User!");
-});
-
-app.delete("/user", (req, res) => {
-  try {
-    const { emailId, phone } = req.query;
-    const message = `The deleted user details - ${emailId} - ${phone}`;
-    fs.appendFile("./userLogs.txt", logMessage(message), (err) => {
-      if (err) {
-        console.log("Error while updating the logs", err);
-        return;
-      }
-    });
-    res.send(
-      `User is deleted! \n The requested email id & phone: ${emailId} - ${phone}`
-    );
-  } catch (err) {
-    console.log(err);
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Middleware 1");
+    // res.send("Request Handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware 2");
+    //res.send("Request Handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Middleware 3");
+    // res.send("Request Handler 3");
+    next();
+  },
+  (req, res) => {
+    console.log("Request Handler 4");
+    res.send("Request Handler 4");
   }
+);
+
+/**
+ * Middlewares can be chained like below also
+ *  "/admin" - route will match the next() of the same routes, it doesn't interfear with "/admin/abc" route
+ *  even if it is in the middle of all "/admin" routes
+ */
+
+app.get("/admin", (req, res, next) => {
+  console.log("Middleware 1");
+  next();
 });
+
+app.get("/admin/abc", (req, res) => {
+  console.log("Request handler, /admin/abc");
+  res.send("Request handler, /admin/abc");
+});
+
+app.get("/admin", (req, res, next) => {
+  console.log("Middleware 2");
+  next();
+});
+
+app.get("/admin", (req, res) => {
+  console.log("Admin: request handler");
+  res.send("Admin: request handler");
+});
+
 app.listen(PROT, () => {
   console.log("Server is up and running!", PROT);
 });
