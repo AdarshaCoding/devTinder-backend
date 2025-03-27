f5c93f72380cdc5cf26a73837b009d488879181c
AdarshaCoding <adarshapc4467@gmail.com>
2025-03-27 06:10:49 +0530

    JWT generated, added & reading from cookies then validated

--- src/app.js
+++ src/app.js
@@ -3,21 +3,21 @@ const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
 const { validateSignupData } = require("./utils/validate");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const PORT = 4000;
 const app = express();
 
 //middleware functions
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   try {
-    validateSignupData(req); //validate the req.body data even before creating the instance of a model
+    validateSignupData(req);
     const { firstName, lastName, emailId, password, age, gender } = req.body;
-
-    // hasing the plain password before storing it in DB
     const passwordHash = await bcrypt.hash(password, 10);
-
     const user = new User({
       firstName,
       lastName,
@@ -26,11 +26,53 @@ app.post("/signup", async (req, res) => {
       age,
       gender,
     });
-
     await user.save();
     res.send(`${user.firstName} Added Succssfully!`);
   } catch (err) {
     res.status(400).send(`ERROR: ${err.message}`);
+  }
+});
+
+//POST : /login
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials!");
+    }
+    const passwordHash = user.password;
+    const isPasswordCorrect = await bcrypt.compare(password, passwordHash);
+
+    if (isPasswordCorrect) {
+      const secretData = jwt.sign({ _id: user._id }, "DEV_SECRET@123");
+      // console.log(secretData);
+      res.cookie("token", secretData);
+
+      res.send(user.firstName + " Login successfull!");
+    } else {
+      throw new Error("Invalid Credential!");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+app.get("/profile", async (req, res) => {
+  try {
+    const cookies = req.cookies; // reading the cookies from client
+    const { token } = cookies; // extract only the token and check it
+    if (!token) {
+      throw new Error("The token is not valid!");
+    }
+    // veriying the token against the SECRET KEY used while generating it
+    const decodedData = jwt.verify(token, "DEV_SECRET@123");
+    const userId = decodedData._id;
+    const user = await User.findById(userId);
+    res.send(user);
+  } catch (err) {
+    res.send("ERROR: " + err.message);
   }
 });
 
